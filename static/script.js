cb504311976d2111e20defb908f22a464bb64570
Sanghamithra Namboodiri <bodhisangh@gmail.com>
2026-04-13 11:08:56 +0530

    overwrite repo with latest local changes

---
+++ static/script.js
@@ -0,0 +1,19 @@
+// Simple alert for form submission
+
+function confirmSubmission() {
+    return confirm("Are you sure you want to submit these marks?");
+}
+
+
+// Highlight rows in table when clicked
+
+function highlightRow(row) {
+    row.style.backgroundColor = "#ffeaa7";
+}
+
+
+// Simple welcome message
+
+function showWelcome(username) {
+    console.log("Welcome " + username);
+}
